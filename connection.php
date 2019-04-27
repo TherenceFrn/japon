a7ec85e25f0bec5b0511618289d6194cb64de4ec
FERRON Therence <therence.ferron@orange.fr>
2019-04-27 15:56:56 +0200

    connexion.php

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$connexion = new PDO('mysql:host=localhost;dbname=japongithub;charset=utf8', 'root', '', array(
+
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+  ));
