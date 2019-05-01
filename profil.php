7539d624925de38b9a945b0d5206d16f20667033
FERRON Therence <therence.ferron@orange.fr>
2019-05-01 17:10:39 +0200

    fin connexion/inscription

--- profil.php
+++ profil.php
@@ -47,13 +47,83 @@
             <p class="card-text"><h5>e-Mail : </h5><?php echo $userinfo-> mail;?></p>
             <p class="card-text"><h5>Numéro d'utilisateur : </h5><?php echo $userinfo-> id;?></p>
 
-            <?php if($userinfo->id == $_SESSION['id']){
+            <?php if(isset($_SESSION['id']) AND $userinfo-> id == $_SESSION['id']){
                 ?>
-                <a href="#" class="card-link">Mes commentaires</a>
-                <a href="#" class="card-link">Créer un article</a>
+                <button type="button" class="btn btn-primary">Editer mon profil</button>
+                <button type="button" class="btn btn-success">Créer un article</button>
+                <a href="deconnexion"><button type="button" class="btn btn-danger float-right">Se déconnecter</button></a>
                 <?php
-              } ?>
+                } ?>
+
+
           </div>
+        </div>
+        <div class="card">
+
+        <div class="alert alert-secondary">
+              Mes commentaires :
+          </div>
+        <table class="table">
+         <thead>
+           <tr>
+             <th>Firstname</th>
+             <th>Lastname</th>
+             <th>Email</th>
+           </tr>
+         </thead>
+         <tbody>
+           <tr>
+             <td>Default</td>
+             <td>Defaultson</td>
+             <td>def@somemail.com</td>
+           </tr>
+           <tr class="table-primary">
+             <td>Primary</td>
+             <td>Joe</td>
+             <td>joe@example.com</td>
+           </tr>
+           <tr class="table-success">
+             <td>Success</td>
+             <td>Doe</td>
+             <td>john@example.com</td>
+           </tr>
+           <tr class="table-danger">
+             <td>Danger</td>
+             <td>Moe</td>
+             <td>mary@example.com</td>
+           </tr>
+           <tr class="table-info">
+             <td>Info</td>
+             <td>Dooley</td>
+             <td>july@example.com</td>
+           </tr>
+           <tr class="table-warning">
+             <td>Warning</td>
+             <td>Refs</td>
+             <td>bo@example.com</td>
+           </tr>
+           <tr class="table-active">
+             <td>Active</td>
+             <td>Activeson</td>
+             <td>act@example.com</td>
+           </tr>
+           <tr class="table-secondary">
+             <td>Secondary</td>
+             <td>Secondson</td>
+             <td>sec@example.com</td>
+           </tr>
+           <tr class="table-light">
+             <td>Light</td>
+             <td>Angie</td>
+             <td>angie@example.com</td>
+           </tr>
+           <tr class="table-dark text-dark">
+             <td>Dark</td>
+             <td>Bo</td>
+             <td>bo@example.com</td>
+           </tr>
+         </tbody>
+        </table>
         </div>
       </div>
 
