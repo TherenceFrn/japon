507be98a59fd65dfbbbc0756c279142aa47145de
FERRON Therence <therence.ferron@orange.fr>
2019-05-01 13:46:09 +0200

    avancemenent

--- inscription.php
+++ inscription.php
@@ -19,6 +19,38 @@
   include 'header.php';
   include 'connection.php';
 
+  if(isset($_POST['validInscription'])){
+
+    if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['pseudo']) AND !empty($_POST['email']) AND !empty($_POST['email2']) AND !empty($_POST['password']) AND !empty($_POST['password2'])){
+
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $email = htmlspecialchars($_POST['email']);
+        $email2 = htmlspecialchars($_POST['email2']);
+        $password = sha1($_POST['password']);
+        $password2 = sha1($_POST['password2']);
+
+        $pseudo_l = strlen($pseudo);
+        $nom_l = strlen($nom);
+        $prenom_l = strlen($prenom);
+        $email_l = strlen($email);
+        $email2_l = strlen($email2);
+
+        if($pseudo_l <= 255 AND $nom_l <= 255 AND $prenom_l <= 255 AND $email_l <= 255 AND $email2_l <= 255){
+
+
+        }else{
+
+          $e = 'Les champs ne doivent pas dépasser 255 caractères';
+        }
+
+    }else{
+
+      $e = 'Veuillez remplir tous les champs';
+    }
+  }
+
   ?>
 
   <!-- main -->
@@ -69,7 +101,7 @@
             <input name="password2" type="password" class="form-control" placeholder="Mot de passe">
           </div>
 
-          <button type="submit" class="btn btn-primary float-right">S'inscrire</button>
+          <button type="submit" name="validInscription" class="btn btn-primary float-right">S'inscrire</button>
 
         </form>
       </div>
@@ -79,8 +111,23 @@
 
   <!-- footer -->
   <?php
-  include 'footer.php'
+  include 'footer.php';
+
   ?>
+  <!-- other -->
+  <?php
+
+  if(isset($e)){
+
+    ?>
+    <script type="text/javascript">
+        console.log('Erreur : '+'<?php echo $e; ?>');
+    </script>
+  <?php
+  }
+
+  ?>
+
 
   <script type="text/javascript" src="script/jquery.js"></script>
   <script type="text/javascript" src="script/script.js"></script>
