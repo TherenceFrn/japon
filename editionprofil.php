8bf6e05dcc198c37bf4289813e93f94f31067888
FERRON Therence <therence.ferron@orange.fr>
2019-05-01 21:07:10 +0200

    favicon

--- editionprofil.php
+++ editionprofil.php
@@ -9,6 +9,10 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="style/style.css" type="text/css"/>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <?php
+
+  include 'favicon.php';
+  ?>
 </head>
 <body>
   <?php
