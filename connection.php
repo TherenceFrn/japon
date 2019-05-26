cc16af868787cecd7a505e31f7d116ddcdeb4a9b
FERRON Therence <therence.ferron@orange.fr>
2019-05-26 09:58:36 +0200

    update

--- connection.php
+++ connection.php
@@ -11,10 +11,10 @@ require('cookie.php');
 ?>
 
 <?php
-$connexion = new PDO('mysql:host=mysql-celbilix.alwaysdata.net;dbname=celbilix_japongithub;charset=utf8', 'celbilix', 'Minecraft49100', array(
+//$connexion = new PDO('mysql:host=mysql-celbilix.alwaysdata.net;dbname=celbilix_japongithub;charset=utf8', 'celbilix', 'Minecraft49100', array(
 
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
-  ));
+//    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+//    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+//  ));
 ?>  
