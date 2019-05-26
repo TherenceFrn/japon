5e382d147a73c64a13509d5d948924bb3cf02163
FERRON Therence <therence.ferron@orange.fr>
2019-05-26 14:26:42 +0200

    footer update

--- footer.php
+++ footer.php
@@ -5,13 +5,13 @@
         <a href="https://twitter.com/therenceferron" target="blank"><i class="fab fa-twitter"></i></a>
       </div>
       <div class="reso">
-        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.instagram.com/therence.ferron/?hl=fr"><i class="fab fa-instagram"></i></a>
       </div>
       <div class="reso">
-        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+        <a href="https://www.linkedin.com/in/therence-ferron/"><i class="fab fa-linkedin-in"></i></a>
       </div>
       <div class="reso">
-        <a href="#"><i class="fab fa-youtube"></i></a>
+        <a href="https://www.youtube.com/channel/UCHPT6CfCSc_a_WJWnBEW_nQ"><i class="fab fa-youtube"></i></a>
       </div>
       <div class="reso">
         <a href="https://www.reddit.com/user/ThatNameWithThatGuy/m/japanmulti/"><i class="fab fa-reddit"></i></a>
