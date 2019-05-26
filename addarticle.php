e8d2458317cb665e2c24346c248eb493f7179c8c
FERRON Therence <therence.ferron@orange.fr>
2019-05-26 22:09:30 +0200

    fin de la journée

--- addarticle.php
+++ addarticle.php
@@ -17,85 +17,34 @@
 
     if(isset($_POST['validArticle']) AND isset($_SESSION['id'])){
 
-      $titre= htmlspecialchars($_POST['nom']);
-      $jour = intval($_POST['jour']);
-      $resume = htmlspecialchars($_POST['resume']);
-      $contenu = htmlspecialchars($_POST['contenu']);
+      if(!empty($_POST['titre']) AND !empty($_POST['jour']) AND !empty($_POST['resume']) AND !empty($_POST['contenu'])){
 
-      if(!empty($_POST['nom']) AND !empty($_POST['jour']) AND !empty($_POST['resume']) AND !empty($_POST['contenu'])){
-
+        $titre= htmlspecialchars($_POST['titre']);
+        $datearticle = date("Y-m-d H:i:s");
+        $auteur = $_SESSION['id'];
+        $jour = intval($_POST['jour']);
+        $avatar = "1.jpg";
+        $contenu = htmlspecialchars($_POST['contenu']);
+        $extrait = htmlspecialchars($_POST['resume']);
 
         $titre_l = strlen($titre);
 
         if($titre_l <= 255){
 
-            if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])){
+          $requeteArticle = $connection->prepare('INSERT INTO articles(titre, datearticle, id_auteur, jour, avatar, contenu, extrait) VALUES(?,?,?,?,?,?,?)');
+          $requeteArticle->execute(array(
+            $titre,
+            $datearticle,
+            $auteur,
+            $jour,
+            $avatar,
+            $contenu,
+            $extrait
+          ));
 
-          $tailleMax = 2097152;
-          $extensionValides = array('jpg','png','jpeg','gif');
-
-            if($_FILES['avatar']['size'] <= $tailleMax){
-
-                $extentionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
-
-                  if(in_array($extentionUpload, $extensionValides)){
-
-                      $chemin = 'images/article/'.$_SESSION['id'].'.'.$extentionUpload;
-                      $resultat = move_uploaded_file($_FILES['avatar']['tmp_name'],$chemin);
-                      if($resultat){
-
-                            $datearticle = date("Y-m-d H:i:s");
-                              
-                            $requeteArticle = $connection->prepare('INSERT INTO articles(titre, datearticle, id_auteur, jour, avatar, contenu, extrait) VALUES(?,?,?,?,?,?,?)');
-                            $requeteArticle->execute(array(
-                              $titre,
-                              $datearticle,
-                              $_SESSION['id'],
-                              $jour,
-                              '1.jpg',
-                              $contenu,
-                              $resume
-                            ));
-
-                            $prereqIm = $connection->prepare("SELECT * FROM articles ORDER BY id DESC LIMIT 1");
-                            $prereqIm->execute();
-                            $infosim = $prereqIm->fetchAll();
-
-                            foreach($infosim as $keyIm){
-                              $avatarLink = $keyIm->id.'.'.$extentionUpload;
-                              $idAvatar = $keyIm->id;
-
-                              $requeteAvatar= $connection->prepare('UPDATE articles SET avatar=:avatar WHERE id=:id');
-                              $requeteAvatar->execute(array(
-                              $avatarLink,
-                              'id' => $_SESSION['id'],
-                              'avatar' => $idAvatar
-                              ));
-
-                            }
-
-
-                      }else{
-
-                        $e = 'Erreur importation fichier';
-                      }
-
-                  }else{
-
-                    $e = 'Format non pris en compte ( jpeg, jpg, png ou gif seulement)';
-                  }
-
-            }else{
-
-              $e = 'Fichiers trop volumineux ( 2 Mo max )';
-            }
         }
-
-	  
-        }
-
+        
       }
-
     }
 
     ?>
@@ -110,7 +59,7 @@
           <form class="" action="" method="post">
 
             <div class="input-group mb-3">
-              <input name="nom" type="text" class="form-control" placeholder="Nom de l'article">
+              <input name="titre" type="text" class="form-control" placeholder="Nom de l'article">
             </div>
 
             <div class="input-group mb-3">
