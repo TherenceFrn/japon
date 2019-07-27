1c45f9b991c416ee1fcc3bff4a14e09ff08bbe79
FERRON Therence <therence.ferron@orange.fr>
2019-07-27 11:08:44 +0200

    test

--- addarticle.php
+++ addarticle.php
@@ -17,6 +17,11 @@
 
     if(!isset($_SESSION['id'])){
       header('Location:connexion.php');
+
+        if($_SESSION['email'] != 'test@test.test'){
+
+        header('Location: index.php');
+      }
     }
 
     if(isset($_POST['validArticle']) AND isset($_SESSION['id'])){
@@ -51,27 +56,54 @@
           $lastid = $connection->lastInsertId();
 
 
-          if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])){
+          // if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])){
             
-            if(exif_imagetype($_FILES['avatar']['tmp_name']) == 1 OR exif_imagetype($_FILES['avatar']['tmp_name']) == 2 OR exif_imagetype($_FILES['avatar']['tmp_name']) == 3 OR exif_imagetype($_FILES['avatar']['tmp_name']) == 18){
+          //   if(exif_imagetype($_FILES['avatar']['tmp_name']) == 1 OR exif_imagetype($_FILES['avatar']['tmp_name']) == 2 OR exif_imagetype($_FILES['avatar']['tmp_name']) == 3 OR exif_imagetype($_FILES['avatar']['tmp_name']) == 18){
               
-              $extentionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));            
+          //     $extentionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));            
               
-              $chemin = 'images/article/'.$lastid.'.'.$extentionUpload;
+          //     $chemin = 'images/article/'.$lastid.'.'.$extentionUpload;
 
-              $avatarPath = $lastid.'.'.$extentionUpload;
+          //     $avatarPath = $lastid.'.'.$extentionUpload;
 
-              move_uploaded_file($_FILES['avatar']['tmp_name'], $chemin);
+          //     move_uploaded_file($_FILES['avatar']['tmp_name'], $chemin);
 
-              $requeteAvatar = $connection->prepare('UPDATE articles SET avatar = ? WHERE id=?');
-              $requeteAvatar->execute(array(
-                $avatarPath,
-                $lastid
-              ));
+          //     $requeteAvatar = $connection->prepare('UPDATE articles SET avatar = ? WHERE id=?');
+          //     $requeteAvatar->execute(array(
+          //       $avatarPath,
+          //       $lastid
+          //     ));
               
+          //   }
+
+          // }// fin add image
+
+          if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])){
+          $tailleMax = 2097152;
+          $extensionValides = array('jpg','png','jpeg','gif');
+            if($_FILES['avatar']['size'] <= $tailleMax){
+                $extentionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.'), 1));
+                  if(in_array($extentionUpload, $extensionValides)){
+                      $chemin = 'images/article/'.$_SESSION['id'].'.'.$extentionUpload;
+                      $resultat = move_uploaded_file($_FILES['avatar']['tmp_name'],$chemin);
+                      if($resultat){
+
+                          $requeteAvatar= $connection->prepare('UPDATE articles SET avatar=:avatar WHERE id=:id');
+                          $requeteAvatar->execute(array(
+                          'avatar' => $_SESSION['id'].'.'.$extentionUpload,
+                          'id' => $_SESSION['id']
+                          ));
+
+                      }else{
+                        $e = 'Erreur importation fichier';
+                      }
+                  }else{
+                    $e = 'Format non pris en compte ( jpeg, jpg, png ou gif seulement)';
+                  }
+            }else{
+              $e = 'Fichiers trop volumineux ( 2 Mo max )';
             }
-
-          }
+        }
 
         }
         
@@ -137,7 +169,7 @@
               <textarea name="contenu" class="form-control editor-contenu-article" placeholder="Contenu de l'article" style="min-height:500px"></textarea>
             </div>
 
-            <button type="submit" name="validArticle" class="btn btn-primary float-right">S'inscrire</button>
+            <button type="submit" name="validArticle" class="btn btn-primary float-right">Ajouter l'article</button>
           </form>
 
           </div>
